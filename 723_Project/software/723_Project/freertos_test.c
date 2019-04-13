caa98f49ec3439e4b246f889e1881daec26ac3ae
Kyung Ho Chun <kchu916@UoA.auckland.ac.nz>
2019-04-13 18:09:52 +1200

    FreqAnalyser done?

--- 723_Project/software/723_Project/freertos_test.c
+++ 723_Project/software/723_Project/freertos_test.c
@@ -1,235 +1,305 @@
 // Standard includes
-#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <system.h>
+#include <io.h>
+#include <math.h>
+#include <stddef.h>
+#include <unistd.h> // for sleep
+
+#include <sys/alt_irq.h>
+
+#include "altera_avalon_pio_regs.h"
+#include "altera_up_avalon_ps2.h"
+#include "altera_up_ps2_keyboard.h"
+#include "altera_up_avalon_video_character_buffer_with_dma.h"
+#include "altera_up_avalon_video_pixel_buffer_dma.h"
 
 // Scheduler includes
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
-#include "freertos/semphr.h"
-
-#include <system.h>
-#include <sys/alt_irq.h>
-#include <io.h>
-#include <altera_avalon_pio_regs.h>
+#include "FreeRTOS/FreeRTOS.h"
+#include "FreeRTOS/task.h"
+#include "FreeRTOS/queue.h"
+#include "FreeRTOS/semphr.h"
+#include "FreeRTOS/portmacro.h"
+#include "FreeRTOS/timers.h"
 
 // Definition of Task Stacks
-#define   TASK_STACKSIZE       2048
+#define SAMPLING_FREQ 16000.0
 
-
-// roc value and frequency value
-//typedef struct{
-//	unsigned int rocValue;
-//	unsigned int newFreq;
-//}freqValues;
+/* RoC value and frequency value */
+typedef struct{
+	double rocValue;
+	double newFreq;
+}freqValues;
 
 // Definition of Task Priorities
-#define PRINT_STATUS_TASK_PRIORITY 14
-#define GETSEM_TASK1_PRIORITY 13
-#define GETSEM_TASK2_PRIORITY 12
-#define RECEIVE_TASK1_PRIORITY 11
-#define RECEIVE_TASK2_PRIORITY 10
-#define SEND_TASK_PRIORITY 9
-#define counter_task_priority 1
-#define lcd_task1_priority 7
-#define freq_relay_priority 8
-
-// Definition of Message Queue
-#define   MSG_QUEUE_SIZE  30
-QueueHandle_t msgqueue;
-
-// Definition of Shed Queue
-#define SHED_QUEUE_SIZE 100
-QueueHandle_t shedqueue;
+#define load_manager_priority 7
+#define switch_con_priority 8
+#define freq_analyser_priority 8
 
 // used to delete a task
 TaskHandle_t xHandle;
 
 // Definition of Semaphore
-SemaphoreHandle_t shared_resource_sem;
+SemaphoreHandle_t roc_sem;
+SemaphoreHandle_t state_sem;
 
 /* Queue Definitions */
-xQueueHandle qNewFreq;
-xQueueHandle qThreshold;
-xQueueHandle qShed;
-xQueueHandle qLoad;
-xQueueHandle qTiming;
-xQueueHandle qResp;
+xQueueHandle Q_freq_data;
+xQueueHandle Q_threshold;
+xQueueHandle Q_shed;
+xQueueHandle Q_load;
+xQueueHandle Q_timing;
+xQueueHandle Q_resp;
 
-// globals variables
-unsigned int number_of_messages_sent = 0;
-unsigned int number_of_messages_received_task1 = 0;
-unsigned int number_of_messages_received_task2 = 0;
-unsigned int getsem_task1_got_sem = 0;
-unsigned int getsem_task2_got_sem = 0;
+/* Globals variables */
+#define SAMPLING_FREQ 16000.0
 char sem_owner_task_name[20];
+static double rocThres = 20;
+static double freqThres = 49;
+int sw_result = 0x00;
 
-void freq_relay(void);
-void NetworkStatusTask();
-void LoadManagerTask();
+void vfreq_relay(void);
 
-static QueueHandle_t Q_freq_data;
+typedef enum
+{
+	normal = 0,
+	shed = 1,
+	maintanence = 2,
+} eMode;
 
 /* Local Function Prototypes */
 int initOSDataStructs(void);
 int initCreateTasks(void);
 
+/* VGA Variables */
+//For frequency plot
+#define FREQPLT_ORI_X 101		//x axis pixel position at the plot origin
+#define FREQPLT_GRID_SIZE_X 5	//pixel separation in the x axis between two data points
+#define FREQPLT_ORI_Y 199.0		//y axis pixel position at the plot origin
+#define FREQPLT_FREQ_RES 20.0	//number of pixels per Hz (y axis scale)
+
+#define ROCPLT_ORI_X 101
+#define ROCPLT_GRID_SIZE_X 5
+#define ROCPLT_ORI_Y 259.0
+#define ROCPLT_ROC_RES 0.5		//number of pixels per Hz/s (y axis scale)
+
+#define MIN_FREQ 45.0 //minimum frequency to draw
+
+#define PRVGADraw_Task_P (tskIDLE_PRIORITY+1)
+TaskHandle_t PRVGADraw;
+
+typedef struct
+{
+	unsigned int x1;
+	unsigned int y1;
+	unsigned int x2;
+	unsigned int y2;
+}Line;
+
+/* LCD Variables */
 xSemaphoreHandle shared_lcd_sem;
 FILE *lcd;
 
 #define ESC 27
 #define CLEAR_LCD_STRING "[2J"
 
-// The next two task compete for a shared resource via a semaphore.  The name of
-// the task that owns the semaphore is copied into the global variable
-// sem_owner_task_name[].
-void getsem_task1(void *pvParameters)
-{
-	while (1)
-	{
-		xSemaphoreTake(shared_resource_sem, portMAX_DELAY);
-		// block forever until receive the mutex
-		strcpy(&sem_owner_task_name[0], "getsem_task1");
-		getsem_task1_got_sem++;
-		xSemaphoreGive(shared_resource_sem);
-		vTaskDelay(100);
+/* VGA Display */
+void PRVGADraw_Task(void *pvParameters ){
+	//initialize VGA controllers
+	alt_up_pixel_buffer_dma_dev *pixel_buf;
+	pixel_buf = alt_up_pixel_buffer_dma_open_dev(VIDEO_PIXEL_BUFFER_DMA_NAME);
+	if(pixel_buf == NULL){
+		printf("can't find pixel buffer device\n");
 	}
-}
+	alt_up_pixel_buffer_dma_clear_screen(pixel_buf, 0);
 
-void getsem_task2(void *pvParameters)
-{
-	while (1)
-	{
-		xSemaphoreTake(shared_resource_sem, portMAX_DELAY);
-		// block forever until receive the mutex
-		strcpy(&sem_owner_task_name[0], "getsem_task2");
-		getsem_task2_got_sem++;
-		xSemaphoreGive(shared_resource_sem);
-		vTaskDelay(130);
+	alt_up_char_buffer_dev *char_buf;
+	char_buf = alt_up_char_buffer_open_dev("/dev/video_character_buffer_with_dma");
+	if(char_buf == NULL){
+		printf("can't find char buffer device\n");
 	}
-}
+	alt_up_char_buffer_clear(char_buf);
 
-// The following task fills up a message queue with incrementing data.  The data
-// is not actually used by the application.  If the queue is full the task is
-// suspended for 1 second.
-void send_task(void *pvParameters)
-{
-	unsigned int msg = 0;
-	while (1)
-	{
-		if (xQueueSend(msgqueue, (void *)&msg, 0) == pdPASS)
-		{
-			// in the message queue
-			msg++;
-			number_of_messages_sent++;
-		}
-		else
-		{
-			vTaskDelay(1000);
-		}
-	}
-}
+	//Set up plot axes
+	alt_up_pixel_buffer_dma_draw_hline(pixel_buf, 100, 590, 200, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
+	alt_up_pixel_buffer_dma_draw_hline(pixel_buf, 100, 590, 300, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
+	alt_up_pixel_buffer_dma_draw_vline(pixel_buf, 100, 50, 200, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
+	alt_up_pixel_buffer_dma_draw_vline(pixel_buf, 100, 220, 300, ((0x3ff << 20) + (0x3ff << 10) + (0x3ff)), 0);
 
-// The next two task pull messages in the queue at different rates.  The number
-// of messages received by the task is incremented when a new message is received
-void receive_task1(void *pvParameters)
-{
-	unsigned int *msg;
-	while (1)
-	{
-		xQueueReceive(msgqueue, &msg, portMAX_DELAY);
-		number_of_messages_received_task1++;
-		vTaskDelay(333);
-	}
-}
+	alt_up_char_buffer_string(char_buf, "Frequency(Hz)", 4, 4);
+	alt_up_char_buffer_string(char_buf, "52", 10, 7);
+	alt_up_char_buffer_string(char_buf, "50", 10, 12);
+	alt_up_char_buffer_string(char_buf, "48", 10, 17);
+	alt_up_char_buffer_string(char_buf, "46", 10, 22);
 
-void receive_task2(void *pvParameters)
-{
-	unsigned int *msg;
-	while (1)
-	{
-		xQueueReceive(msgqueue, &msg, portMAX_DELAY);
-		number_of_messages_received_task2++;
-		vTaskDelay(1000);
-	}
-}
+	alt_up_char_buffer_string(char_buf, "df/dt(Hz/s)", 4, 26);
+	alt_up_char_buffer_string(char_buf, "60", 10, 28);
+	alt_up_char_buffer_string(char_buf, "30", 10, 30);
+	alt_up_char_buffer_string(char_buf, "0", 10, 32);
+	alt_up_char_buffer_string(char_buf, "-30", 9, 34);
+	alt_up_char_buffer_string(char_buf, "-60", 9, 36);
 
-void lcd_task1(void* pvParameters)
- {
-	printf("STARTING LCD");
+	double freq[100], dfreq[100];
+	int i = 99, j = 0;
+	Line line_freq, line_roc;
+
 	while(1){
-	  IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, 0x55);
-	  usleep(1000000);
-	  IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, 0xaa);
-	  usleep(1000000);
-	}
- }
 
-void freq_relay(){
-	unsigned int temp = IORD(FREQUENCY_ANALYSER_BASE, 0);
-	//printf("%f Hz\n", 16000/(double)temp);
-	xQueueSendToBackFromISR( Q_freq_data, &temp, pdFALSE );
+		//receive frequency data from queue
+		while(uxQueueMessagesWaiting( Q_freq_data ) != 0){
+			xQueueReceive( Q_freq_data, freq+i, 0 );
+
+			//calculate frequency RoC
+
+			if(i==0){
+				dfreq[0] = (freq[0]-freq[99]) * 2.0 * freq[0] * freq[99] / (freq[0]+freq[99]);
+			}
+			else{
+				dfreq[i] = (freq[i]-freq[i-1]) * 2.0 * freq[i]* freq[i-1] / (freq[i]+freq[i-1]);
+			}
+
+			if (dfreq[i] > 100.0){
+				dfreq[i] = 100.0;
+			}
+
+
+			i =	++i%100; //point to the next data (oldest) to be overwritten
+
+		}
+
+		//clear old graph to draw new graph
+		alt_up_pixel_buffer_dma_draw_box(pixel_buf, 101, 0, 639, 199, 0, 0);
+		alt_up_pixel_buffer_dma_draw_box(pixel_buf, 101, 201, 639, 299, 0, 0);
+
+		for(j=0;j<99;++j){ //i here points to the oldest data, j loops through all the data to be drawn on VGA
+			if (((int)(freq[(i+j)%100]) > MIN_FREQ) && ((int)(freq[(i+j+1)%100]) > MIN_FREQ)){
+				//Calculate coordinates of the two data points to draw a line in between
+				//Frequency plot
+				line_freq.x1 = FREQPLT_ORI_X + FREQPLT_GRID_SIZE_X * j;
+				line_freq.y1 = (int)(FREQPLT_ORI_Y - FREQPLT_FREQ_RES * (freq[(i+j)%100] - MIN_FREQ));
+
+				line_freq.x2 = FREQPLT_ORI_X + FREQPLT_GRID_SIZE_X * (j + 1);
+				line_freq.y2 = (int)(FREQPLT_ORI_Y - FREQPLT_FREQ_RES * (freq[(i+j+1)%100] - MIN_FREQ));
+
+				//Frequency RoC plot
+				line_roc.x1 = ROCPLT_ORI_X + ROCPLT_GRID_SIZE_X * j;
+				line_roc.y1 = (int)(ROCPLT_ORI_Y - ROCPLT_ROC_RES * dfreq[(i+j)%100]);
+
+				line_roc.x2 = ROCPLT_ORI_X + ROCPLT_GRID_SIZE_X * (j + 1);
+				line_roc.y2 = (int)(ROCPLT_ORI_Y - ROCPLT_ROC_RES * dfreq[(i+j+1)%100]);
+
+				//Draw
+				alt_up_pixel_buffer_dma_draw_line(pixel_buf, line_freq.x1, line_freq.y1, line_freq.x2, line_freq.y2, 0x3ff << 0, 0);
+				alt_up_pixel_buffer_dma_draw_line(pixel_buf, line_roc.x1, line_roc.y1, line_roc.x2, line_roc.y2, 0x3ff << 0, 0);
+			}
+		}
+		vTaskDelay(10);
+
+	}
+}
+
+/* Keyboard ISR */
+void ps2_isr(void* ps2_device, alt_u32 id){
+	unsigned char byte;
+	alt_up_ps2_read_data_byte_timeout(ps2_device, &byte);
+	xQueueSendToBackFromISR( Q_threshold, &byte, pdFALSE );
+}
+
+/* Push button ISR */
+void push_button_irq(){
+	IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, IORD_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE));
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE, 0x7); //write 1 to clear all detected falling edges
 	return;
 }
 
-void NetworkStatusTask(){
-	double freq[100], dfreq[100];
-	int i = 99, j = 0;
+/* Read incoming frequency from ISR*/
+void vfreq_relay()
+{
+	double freq = SAMPLING_FREQ / IORD(FREQUENCY_ANALYSER_BASE, 0);
 
-	/* threshold test purpose */
-	double rocFreq = 7.0;
-	double lowerFreq = 48.2;
-	unsigned int false = 0;
-	while(1){
+	// Send calculation tasks
+	xQueueSendToBackFromISR( Q_freq_data, &freq, pdFALSE );
+	//xSemaphoreGiveFromISR( vNetworkStatusTask, NULL);
+	return;
+}
 
-		/* receive frequency data from queue */
-		while(uxQueueMessagesWaiting( Q_freq_data ) != 0)
+/* Read the frequency from the frequency relay */
+void vFrequencyAnalyser()
+{
+	freqValues freqValues;
+	double freqPrev = 50.0;
+
+	while(1)
+	{
+		if(xSemaphoreTake(roc_sem, portMAX_DELAY))
 		{
-			xQueueReceive( Q_freq_data, freq+i, 0 );
+			// Do calculations
+			freqValues.newFreq = SAMPLING_FREQ / IORD(FREQUENCY_ANALYSER_BASE, 0);
+			freqValues.rocValue = (freqValues.newFreq-freqPrev) / (freqValues.newFreq);
+			freqPrev = freqValues.newFreq;
 
-			/* calculate frequency RoC */
-
-			if(i==0)
-			{
-
-				dfreq[0] = (freq[0]-freq[99]) * 2.0 * freq[0] * freq[99] / (freq[0]+freq[99]); //a special case calculation because if you do i-1 on 0
-			}
-			else
-			{
-				dfreq[i] = (freq[i]-freq[i-1]) * 2.0 * freq[i]* freq[i-1] / (freq[i]+freq[i-1]); //normal rate of change calculation
-			}
-
-			if (dfreq[i] > 100.0)
-			{
-				dfreq[i] = 100.0; /* maximum rate of change is 100 */
-			}
-
-			/* compare arbitrary thresholds with RoC and newly calculated frequency to test end to shedQ
-			if RoC exceeds frequency value or frequency values is less than the lower bound */
-			if ((fabs(rocFreq) < dfreq[i]) || lowerFreq > freq[i]  )
-			{
-				xQueueSendToBack(shedqueue, (void*)&false, pdFALSE);
-			}
-			i =	++i%100;  /* point to the next data (oldest) to be overwritten */
+			//Send new Data to the Queue
+			xQueueSend(Q_freq_data, &freqValues, 0);
+			xSemaphoreGive(roc_sem);
 		}
 	}
 }
 
-void LoadManagerTask(){
-//xQueueReceive();
+/* Read the frequency from the*/
+void vNetworkStatusTask()
+{
 
-	//IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE,  )
+	while(1)
+	{
+
+	}
+}
+
+
+/* Switch controller */
+void vSwitchCon(void *pvParameters)
+{
+	while(1)
+	{
+		sw_result = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & 0x00FF; //read slide switches
+		IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE, sw_result);    //light red LEDs according to switch positions
+	}
+}
+
+/* Load manager */
+void vLoadManagerTask()
+{
+	while(1)
+	{
+
+	}
 }
 
 int main(int argc, char* argv[], char* envp[])
 {
-	alt_irq_register(FREQUENCY_ANALYSER_IRQ, 0, freq_relay);
+	// Start Frequency ISR
+	alt_irq_register(FREQUENCY_ANALYSER_IRQ, 0, vfreq_relay);
+
+	// Start Keyboard setup and ISR
+	alt_up_ps2_dev * ps2_device = alt_up_ps2_open_dev(PS2_NAME);
+	if(ps2_device == NULL){
+		printf("can't find PS/2 device\n");
+		return 1;
+	}
+	alt_up_ps2_enable_read_interrupt(ps2_device);
+	alt_irq_register(PS2_IRQ, ps2_device, ps2_isr);
+
+	//Push button setup
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PUSH_BUTTON_BASE, 0x7); //enable interrupt for all three push buttons (Keys 1-3 -> bits 0-2)
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE, 0x7); //write 1 to edge capture to clear pending interrupts
+	alt_irq_register(PUSH_BUTTON_IRQ, 0, push_button_irq);  //register ISR for push button interrupt request
+
 	initOSDataStructs();
 	initCreateTasks();
 	vTaskStartScheduler();
-	for (;;);
+
+	for(;;);
 	return 0;
 }
 
@@ -237,33 +307,25 @@ int main(int argc, char* argv[], char* envp[])
 int initOSDataStructs(void)
 {
 	/* Create Queues */
-	qNewFreq = xQueueCreate( 100, sizeof( double ) );
-	qThreshold = xQueueCreate( 2, sizeof( double ) );
-	qShed = xQueueCreate( 10, sizeof( int ) );
-	qLoad = xQueueCreate( 10, sizeof( int ) );
-	qTiming = xQueueCreate( MSG_QUEUE_SIZE, sizeof( void* ) );
-	qResp = xQueueCreate( MSG_QUEUE_SIZE, sizeof( void* ) );
-
-	msgqueue = xQueueCreate( MSG_QUEUE_SIZE, sizeof( void* ) );
-	shedqueue= xQueueCreate( SHED_QUEUE_SIZE, sizeof(double));
-
+	Q_freq_data = xQueueCreate( 100, sizeof( double ) );
+	Q_threshold = xQueueCreate( 2, sizeof( double ) );
+	Q_shed = xQueueCreate( 10, sizeof( int ) );
+	Q_load = xQueueCreate( 10, sizeof( int ) );
+	Q_timing = xQueueCreate( 4, sizeof( int ) );
+	Q_resp = xQueueCreate( 1, sizeof( int ) );
 
 	/* Create Semaphores */
-	shared_resource_sem = xSemaphoreCreateCounting( 9999, 1 );
-	shared_lcd_sem = xSemaphoreCreateCounting( 9999, 1 );
+	roc_sem = xSemaphoreCreateCounting( 9999, 1 );
+	state_sem = xSemaphoreCreateCounting( 9999, 1 );
 
 	return 0;
 }
-
 // This function creates the tasks used in this example
 int initCreateTasks(void)
 {
-	xTaskCreate(getsem_task1, "getsem_task1", TASK_STACKSIZE, NULL, GETSEM_TASK1_PRIORITY, NULL);
-	xTaskCreate(getsem_task2, "getsem_task2", TASK_STACKSIZE, NULL, GETSEM_TASK2_PRIORITY, NULL);
-	xTaskCreate(receive_task1, "receive_task1", TASK_STACKSIZE, NULL, RECEIVE_TASK1_PRIORITY, NULL);
-	xTaskCreate(receive_task2, "receive_task2", TASK_STACKSIZE, NULL, RECEIVE_TASK2_PRIORITY, NULL);
-	xTaskCreate(send_task, "send_task", TASK_STACKSIZE, NULL, SEND_TASK_PRIORITY, NULL);
-	xTaskCreate(lcd_task1, "lcd_task1",	TASK_STACKSIZE,	NULL, lcd_task1_priority, NULL);
-	xTaskCreate(freq_relay, "freq_relay",	TASK_STACKSIZE,	NULL, freq_relay_priority, NULL);
+	xTaskCreate( vLoadManagerTask, "LoadManagerTask", configMINIMAL_STACK_SIZE, NULL, load_manager_priority, NULL );
+	xTaskCreate( vSwitchCon, "SwitchCon", configMINIMAL_STACK_SIZE, NULL, switch_con_priority, NULL );
+	xTaskCreate( vFrequencyAnalyser, "FrequencyAnalyser", configMINIMAL_STACK_SIZE, NULL, freq_analyser_priority, NULL );
+
 	return 0;
 }
